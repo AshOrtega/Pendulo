81396d070c95657d786447c2cc84bbd469359e97
AshOrtega <aschortega@gmail.com>
2017-11-29 12:12:47 +0100

    Buenas, subo el ejercicio Pendulo

---
+++ IDE/src/Pendulo.java
@@ -0,0 +1,19 @@
+/**
+ * Clase que proporciona métodos relacionados con un péndulo
+ * @author Sergio
+ * @version beta
+ */
+public class Pendulo {
+	 // Constante que representa la aceleración de la gravedad terrestre
+	
+	public static final double GRAVEDAD = 9.8;
+	/* 
+	 * Método que calcula el periodo del péndulo 
+	 * @param longitud, que es la longitud del péndulo
+	 * @return double, representando el periodo de oscilación
+	 */
+	
+	public static double calcularPeriodo (double longitud) {
+		return 2 * Math.PI * Math.sqrt(longitud/GRAVEDAD);
+	}
+}
